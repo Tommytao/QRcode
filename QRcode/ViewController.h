ae623b9f5a9abb2cf617c6fcd23d56d5600e0b26
Tommy <tao941240@gmail.com>
2017-03-07 13:16:18 +0800

    Initial Commit

---
+++ QRcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRcode
+//
+//  Created by Tommy on 2017/3/7.
+//  Copyright © 2017年 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
